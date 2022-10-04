75df861a9c732fb97562b75ecf25f2f117dd09e2
Diego-Rapha <codrix43@gmail.com>
2022-10-04 01:19:12 -0300

    Criação da pasta Introdução ao python e criação de dois arquivos .py: primeiro_programa_py e tipos

---
+++ unimed-dio/introducao_ao_python/tipos.py
@@ -0,0 +1,5 @@
+print(11 * 10 * 1000, 'é uma representação do tipo integer.')
+print(1.5 + 1 + 0.5, 'é uma representação do tipo float.')
+print(True, 'é uma representação do tipo boolean.')
+print(False, 'é uma representação do tipo boolean.')
+print("Python", 'é uma representação de string, assim como o texto que acabas de ler.')
